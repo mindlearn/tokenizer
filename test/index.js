bc6476982ef5caace983862a0ee8af1b4556d0e2
TROLLGAMES <84880155+GsLKS@users.noreply.github.com>
2023-08-27 01:40:11 -0300

    Create index.js

---
+++ test/index.js
@@ -0,0 +1,5 @@
+// const tokenizer = require("@mindlearn/tokenizer");
+const tokenizer = require("../");
+
+const word = "Hello World, here is a basic mindscript tokenizer example.";
+console.log(tokenizer(word));
